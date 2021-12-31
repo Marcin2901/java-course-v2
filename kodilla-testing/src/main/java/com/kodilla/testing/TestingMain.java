fac22b4b3928a3b76a486f40182561b74ce87346
Marcin <jamarcin2901@wp.pl>
2021-12-31 22:43:56 +0100

    Creating first tests for username and calculator methods

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
 
+        //  TEST#1
+        SimpleUser simpleUser = new SimpleUser("Marcin");
+        String result = simpleUser.getUSername();
+        if (result.equals("Marcin")) {
+            System.out.println("Test 1 OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        //  TEST#2
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(3, 11);
+        if(addResult == 14) {
+            System.out.println("Test 2 OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        // Test#3
+        int subResult = calculator.subtract(6, 2);
+        if(subResult == 4) {
+            System.out.println("Test 3 OK");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
